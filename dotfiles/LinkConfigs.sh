7e797f0f5b94e0957febd320950f95fcc66bc7b4
freshLiver <freshliver.cys@gmail.com>
2022-05-02 20:56:48 +0800

    mv fcitx5 theme configs to here

--- dotfiles/LinkConfigs.sh
+++ dotfiles/LinkConfigs.sh
@@ -1,11 +1,14 @@
 # !/bin/bash
 HOME_CONFIG="$HOME/.config"
+HOME_LOCAL_SHARE="$HOME/.local/share"
+
 mkdir -p "$HOME_CONFIG"
+mkdir -p "$HOME_LOCAL_SHARE"
+
+
 
 # tmux -- .tmux.conf only
 ln -srf ./shell/.tmux.conf "$HOME/.tmux.conf"
-
-
 
 # zsh -- .zshrc and .oh-my-zsh/themes/theme.zsh-theme
 ZSH_TEHEM_DIR="$HOME/.oh-my-zsh/themes"
@@ -14,10 +17,11 @@ mkdir -p "$ZSH_TEHEM_DIR"
 ln -srf ./shell/zsh/.zshrc "$HOME/.zshrc"
 ln -srf ./shell/zsh/mytheme.zsh-theme "$ZSH_TEHEM_DIR"
 
-
-
 # yakuake -- .config/yakuakerc
 ln -srf ./shell/yakuakerc "$HOME_CONFIG"
+
+
+
 
 
 # vim -- .vimrc and .vim/colors
@@ -26,6 +30,16 @@ mkdir -p "$VIM_THEME_DIR"
 
 ln -srf ./vim/.vimrc "$HOME/.vimrc"
 ln -nsrf ./vim/colors/ "$VIM_THEME_DIR"
+
+
+
+
+
+# fcitx5 themes -- whole directory
+FCITX5_THEME_DIR="$HOME_LOCAL_SHARE/fcitx5"
+mkdir -p "$FCITX5_THEME_DIR"
+
+ln -nsrf ./fcitx5/themes/ "$FCITX5_THEME_DIR"
 
 
 
