aca4f462dee8eb98fd06db784ec91207b46433b6
freshLiver <freshliver.cys@gmail.com>
2022-07-13 00:13:53 +0800

    rm old configs

--- Linux/KDE/com.github.freshliver.commandPanel/copy2TemplateDir.sh
+++
@@ -1,5 +0,0 @@
-DirName="$(basename $(readlink -f . | sed -r 's/ /@@/g') | sed -r 's/@@/ /g')"
-RelativePath="../$DirName/"
-TargetDir="$(readlink -f ~/.local/share/plasma/layout-templates/$DirName)"
-
-cp -Tr $RelativePath $TargetDir
--- Linux/KDE/konsole/Setting.profile
+++
@@ -1,9 +0,0 @@
-[Appearance]
-ColorScheme=DarkPastels
-Font=JetBrains Mono,14,-1,5,57,0,0,0,0,0
-
-[General]
-Name=Setting
-Parent=FALLBACK/
-TerminalColumns=120
-TerminalRows=30
