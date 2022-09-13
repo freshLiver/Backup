fdfbae5fadea8d11f2434340d05e7681f46c990f
freshliver <freshliver.cys@gmail.com>
2022-09-13 10:58:13 +0800

    Add Kprobe draft script

---
+++ Linux/scripts/kernel/Kprobe/py-read-img.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# ftrace configs
+
+# kprobe configs
+
+# enable ftrace
+echo 1 > tracing_on
+
+# read image using python
+
+# disable ftrace
+echo 0 > tracing_on
+
+# output trace log
+
