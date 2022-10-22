497cc5372d1c45e51c0f70e3c0f64430f27393ec
freshliver <freshliver.cys@gmail.com>
2022-10-22 13:03:48 +0800

    Update single line usage

--- Linux/scripts/clipboard/trims.sh
+++ Linux/scripts/clipboard/trims.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+# single line command for Klipper
+# xclip -o | tr '\n' ' ' | tr -s ' ' | xclip -selection clipboard
+
 # this script remove newlines and repeat spaces in the input text
 TEXT="$1"
 RESULT="$(echo "$TEXT" | tr '\n' ' ' | tr -s ' ')"
