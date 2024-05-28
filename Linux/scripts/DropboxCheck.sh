ed2d816094865b7bd5b2050bb8427d40f693998d
freshliver <freshliver.cys@gmail.com>
2024-05-28 22:57:57 +0800

    Update dropbox and sysinfo scripts

--- Linux/scripts/DropboxCheck.sh
+++ Linux/scripts/DropboxCheck.sh
@@ -2,6 +2,7 @@ dropboxPID="$(ps aux | grep dropbox | grep home | awk '{print $2}')"
 
 if [ "$dropboxPID" = "" ]; then
     echo "Restart Dropbox !!"
+    nohup $HOME/.dropbox-dist/dropboxd | systemd-cat &
 else
     echo ""
 fi
