a2897a3b86b1714589f56a5cf97be080f87022be
freshliver <freshliver.cys@gmail.com>
2024-08-11 18:30:46 +0800

    APP: custom mime type for drawio

--- Linux/configs/apps/LinkApps.sh
+++ Linux/configs/apps/LinkApps.sh
@@ -8,3 +8,8 @@ ln -srf ./icons/* "$APP_LOCAL_BASE_PATH/icons"
 # put app entries at $HOME/.local/share/applications
 mkdir -p "$APP_LOCAL_BASE_PATH/applications"
 ln -srf ./entries/* "$APP_LOCAL_BASE_PATH/applications"
+
+
+xdg-mime install --mode user ./mime/drawio-drawio.xml
+xdg-icon-resource install --context mimetypes --size 128 ./icons/drawio.png text-x-drawio
+update-mime-database ~/.local/share/mime/
