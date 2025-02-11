70c016eb7a75b5ae70d1da83a53cda5676f88509
freshliver <freshliver.cys@gmail.com>
2025-02-11 21:38:51 +0800

    Update dropbox script

--- Linux/scripts/DropboxCheck.sh
+++ Linux/scripts/DropboxCheck.sh
@@ -2,7 +2,8 @@ dropboxPID="$(ps aux | grep dropbox | grep home | awk '{print $2}')"
 
 if [ "$dropboxPID" = "" ]; then
     echo "Restart Dropbox !!"
-    nohup $HOME/.dropbox-dist/dropboxd | systemd-cat &
+    $HOME/.dropbox-dist/dropboxd | systemd-cat &
+    disown
 else
     dropbox stop
     sleep 5
