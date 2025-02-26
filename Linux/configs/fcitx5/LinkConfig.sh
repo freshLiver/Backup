973ae95ac2d6c0913d654115bf3998420cc6ae96
freshliver <freshliver.cys@gmail.com>
2025-02-27 00:35:33 +0800

    add envvars for fcitx5 on wayland

--- Linux/configs/fcitx5/LinkConfig.sh
+++ Linux/configs/fcitx5/LinkConfig.sh
@@ -3,6 +3,10 @@
 FCITX5_CONFIG_DIR="$HOME/.local/share/fcitx5"
 FCITX5_THEME_DIR="$FCITX5_CONFIG_DIR/themes"
 
+WAYLAND_CONFIG_ENVDIRS="$HOME/.config/environment.d/"
 
 mkdir -p $FCITX5_THEME_DIR
 ln -nsrf ./themes/Material-Color-teal-dark $FCITX5_THEME_DIR
+
+mkdir -p "$WAYLAND_CONFIG_ENVDIRS"
+ln -srf wayland-envvars.conf "$WAYLAND_CONFIG_ENVDIRS"
