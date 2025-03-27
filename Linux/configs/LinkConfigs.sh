89f47324d82f71fd8897c604647a817680d5a7cd
freshliver <freshliver.cys@gmail.com>
2025-03-27 16:12:20 +0800

    Add progress messages

--- Linux/configs/LinkConfigs.sh
+++ Linux/configs/LinkConfigs.sh
@@ -1,28 +1,29 @@
-# !/bin/bash
-HOME_CONFIG="$HOME/.config"
-HOME_LOCAL_SHARE="$HOME/.local/share"
-
-mkdir -p "$HOME_CONFIG"
-mkdir -p "$HOME_LOCAL_SHARE"
-
+#!/bin/bash
 
 # system configs
 bash -c "cd ./system/ && ./LinkSystemConfigs.sh"
+echo "all system configs linked!"
 
 # shell/terminal configs
 bash -c "cd ./shell/ && ./LinkShellConfigs.sh"
+echo "all shell configs linked!"
 
 # coding configs
 bash -c "cd ./coding/ && ./LinkCodingConfigs.sh"
+echo "all coding configs linked!"
 
 # systemd-service configs
 bash -c "cd ./services/ && ./install-services.sh"
+echo "all systemd-services installed!"
 
 # multimedia configs
 bash -c "cd ./media/ && ./LinkMediaConfigs.sh"
+echo "all multi-media configs linked!"
 
 # desktop entries
 bash -c "cd ./apps/ && ./LinkApps.sh"
+echo "all app entries/icons linked!"
 
 # utils
 bash -c "cd ./utils/ && ./LinkUtilsConfigs.sh"
+echo "all util configs linked!"
